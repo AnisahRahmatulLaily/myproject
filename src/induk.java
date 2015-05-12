90d7081572d5f9fe7172a7cd847b039b64751c4b
AnisahRahmatulLaily <anisah.r.laily@gmail.com>
2015-05-12 07:52:02 +0700

    ini

---
+++ src/induk.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class induk extends ruang{
+    S
+    void input_identitas_ruang_kelas(String nama_ruang, String lokasi_ruang, String fakultas) {
+		System.out.println("input ruang kelas");
+		      setNama_ruang(in.next());
+		System.out.println("input lokasi ruang kelas");
+		lokasi_ruang=in.next();
+		System.out.println("pilih fakultas");
+		fakultas=in.next();
+                
+    
+}
